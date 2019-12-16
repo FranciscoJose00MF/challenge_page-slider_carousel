2fd48474d14dd8c6a9555e0d20ff263fc23ce8e1
franciscodomingosjose06798@gmail.com <francesnerlandes@gmail.com>
2019-12-16 15:22:43 +0100

    update

--- lib/screens/getting_started_screen.dart
+++ lib/screens/getting_started_screen.dart
@@ -1,11 +1,49 @@
 import 'package:challenge_page_slider_carousel/model/slide.dart';
 import 'package:challenge_page_slider_carousel/screens/widgets/slideItem.dart';
+import 'package:challenge_page_slider_carousel/screens/widgets/slide_dots.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class GettingStartedScreen extends StatelessWidget {
+class GettingStartedScreen extends StatefulWidget {
+  @override
+  _GettingStartedScreenState createState() => _GettingStartedScreenState();
+}
+
+class _GettingStartedScreenState extends State<GettingStartedScreen> {
+  int _currentPage=0;
   final PageController _pageController = PageController(
     initialPage: 0
   );
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 5), (Timer timer){
+      if(_currentPage<2){
+        _currentPage++;
+      }else{
+        _currentPage=0;
+      }
+
+      _pageController.animateToPage(_currentPage,
+      duration:
+        Duration(milliseconds: 300,),
+        curve: Curves.easeIn);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  _onPageChange(int index){
+    setState(() {
+      _currentPage=index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +56,34 @@ class GettingStartedScreen extends StatelessWidget {
           children: <Widget>[
 
             Expanded(
-              child: PageView.builder(
-                controller: _pageController,
-                scrollDirection: Axis.horizontal,
-                itemCount: slideList.length,
-                itemBuilder: (ctx,i)=>SlideItem(i),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  PageView.builder(
+                  controller: _pageController,
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: _onPageChange,
+                  itemCount: slideList.length,
+                  itemBuilder: (ctx,i)=>SlideItem(i),
+                ),
+                  Stack(
+
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 35),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                          for(int i=0; i<slideList.length; i++ )
+                            if(i==_currentPage)
+                              SlideDots(true)
+                            else
+                              SlideDots(false)
+                        ],),
+                      )
+                    ],
+                  ),
+                              ]
               ),
             ),
 
